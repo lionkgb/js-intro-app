96053fd43431d4dbf7c188d094aedd939062254b
lionkgb <lionceau.lk@gmail.com>
2017-02-22 13:12:39 +0200

    Onclik loop  function

--- app/assets/javascripts/guess_a_box.js
+++ app/assets/javascripts/guess_a_box.js
@@ -1,4 +1,4 @@
-var winningBox = Math.floor((Math.random() * 3) + 1); // Picks a random number between 1 and 3:
+var winningBox = Math.floor((Math.random() * 1) + 1); // Picks a random number between 1 and 3:
 
 function guessBox(boxNumber) {
 
--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,1 +1,10 @@
-
+var francis = 1;
+function jackHammer() {
+  if (francis === 5){
+    alert("You win!");
+  } else {
+    console.log("Keep Cliking")
+    francis++;
+  }
+}
+  
